20a5d35c122993a5c07751eb422965cc7633ac33
AzureAD\ArchitBhardwaj <archit.bhardwaj@openecx.com>
2021-11-25 17:00:26 +0000

    new exception class for max length

---
+++ src/PasswordVerifier/PasswordMaxLengthException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace KataExceptions
+{
+    public class PasswordMaxLengthException : Exception
+    {
+        public PasswordMaxLengthException()
+        {
+        }
+
+        public PasswordMaxLengthException(string message)
+            : base(message)
+        {
+        }
+
+        public PasswordMaxLengthException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
